33a43b44d7fec6195b09d8b50e5e0171a5ba528c
Yang Cao <yang.cao@energyworx.org>
2023-10-25 14:16:58 +0200

    explore timegpt on uk utility data

---
+++ main.py
@@ -0,0 +1,50 @@
+import json
+import os
+import pandas as pd
+from nixtlats import TimeGPT
+from utils import create_train_val_test_split, evaluate_forecast, gap_check
+import plotly.io as pio
+pio.renderers.default = "browser"
+
+timegpt = TimeGPT(token=os.environ['TIMEGPT_TOKEN'])
+
+# timegpt.validate_token()
+# df = pd.read_csv('https://raw.githubusercontent.com/Nixtla/transfer-learning-time-series/main/datasets/air_passengers.csv')
+# df.head()
+# fig = timegpt.plot(df, time_col='timestamp', target_col='value')
+# fig.show()
+
+data_path = 'data'
+file_name = "MAC000100"
+selected_customers = ["MAC000119", "MAC000121", "MAC000117", "MAC000110", "MAC000108"]
+figs = {}
+for file_name in selected_customers:
+    df = pd.read_csv(f"{data_path}/{file_name}.csv", index_col='DateTime',
+                        parse_dates=True, names=['DateTime', 'customer_id', 'usage'],
+                        header=0)
+    df['usage'] = pd.to_numeric(df['usage'], errors='coerce')
+    df = df[['usage']]
+    df_extended = gap_check(df=df, column_name='usage', freq='30T')
+    df_extended_cleaned = df_extended.ffill().bfill()
+    gap_check(df=df_extended_cleaned, column_name='usage', freq='30T')
+    df = df_extended_cleaned
+    df.index = pd.DatetimeIndex(df.index, freq='30T')
+    df['DateTime'] = df.index
+    print(f"[{file_name}]: entire data set: {df.describe()}")
+
+    train, test = create_train_val_test_split(df)
+    # print(f"[{file_name}]: known historical data length: {len(train)}, forecast horizon: {len(test)}")
+    actual_horizon = 48
+    print(f"[{file_name}]: known historical data length: {len(train)}, forecast horizon: {actual_horizon}")
+    timegpt_fcst_df = timegpt.forecast(df=train, finetune_steps=50, h=actual_horizon, time_col='DateTime', target_col='usage', freq='30T')
+    fig_name = f"{file_name}_timegpt_forecast"
+    figs[fig_name] = timegpt.plot(train.iloc[-actual_horizon*28::], timegpt_fcst_df, time_col='DateTime', target_col='usage')
+    figs[fig_name].show()
+    figs[fig_name].savefig(f"results/{fig_name}.png")
+
+    test_set_result = evaluate_forecast(test['usage'].iloc[-actual_horizon::], timegpt_fcst_df['TimeGPT'])
+    print(f"[{file_name}]: testing result rmse: {test_set_result['rmse']}, mape: {test_set_result['mape']:.3%}")
+    test_set_result.pop('mape_raw')
+    test_set_result.pop('rmse_raw')
+    with open(f"results/{file_name}_timegpt_forecast_metrics.txt", "w") as f:
+        json.dump(test_set_result, f)
---
+++ utils.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error
+
+
+def gap_check(df, column_name, start=None, end=None, freq='H'):
+    """Fill the df with indexes of expected freq and return the dataframe with NaN values (gaps)
+    """
+    start = start or df.index.min()
+    end = end or df.index.max()
+    expected_index = pd.date_range(start=start, end=end, freq=freq)
+    df_extended = df.reindex(expected_index, copy=False)
+    # print(df_extended.loc[df_extended[column_name].isna()])
+    # print(f'length of gaps: {len(df_extended.loc[df_extended[column_name].isna()])}')
+    return df_extended
+
+def create_train_val_test_split(dataframe, train_start=None, train_end=None, test_start=None, test_end=None):
+    """
+    Ignore the validation set because we will do cross validation on the entire training set to aovid overfiting the validation set
+    """
+    train_end = train_end or int(0.85 * len(dataframe))
+    train_start = train_start or 0
+    test_end = test_end or -1
+    test_start = test_start or train_end + 1
+
+    train_df = dataframe.iloc[train_start:train_end]
+    test_df = dataframe.iloc[test_start:test_end]
+    return train_df, test_df
+
+
+def evaluate_forecast(y_true, y_pred):
+    """
+    y_true (np.array): m * n matrix, m = # of samples, n = # of targets, e.g. forecast 3 days ahead = 72 hours, n=72.
+    y_pred (np.array):
+    metric (str): 'RMSE' = 'root_mean_squared_error' or 'MAPE' = mean_absolute_percentage_error
+    """
+    rmse = mean_squared_error(y_true, y_pred, squared=False)
+    mape = mean_absolute_percentage_error(y_true, y_pred)
+
+    rmse_raw = mean_squared_error(y_true, y_pred, squared=False, multioutput='raw_values')
+    mape_raw = mean_absolute_percentage_error(y_true, y_pred, multioutput='raw_values')
+    return {'rmse': rmse, 'mape': mape, 'rmse_raw': rmse_raw, 'mape_raw': mape_raw}
